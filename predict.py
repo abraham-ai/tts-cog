3310fdd2eee08cc9f6c1fed13ad794e5c08eaab6
Gene Kogan <kogan.gene@gmail.com>
2023-01-22 18:42:33 -0800

    simplify

--- predict.py
+++ predict.py
@@ -1,22 +1,26 @@
-from cog import BasePredictor, Input
-
 import sys
 import os
-import torch
-import torchaudio
 
-sys.path.extend([
-    "/src/tortoise-tts/tortoise",
-])
-
+sys.path.append("/src/tortoise-tts/tortoise")
 os.environ["TORCH_HOME"] = "/src/.torch"
 os.environ['TRANSFORMERS_CACHE'] = '/src/.cache/'
 
+from cog import BasePredictor, Input, Path
+import tempfile
+import torch
+import torchaudio
 from api import TextToSpeech
 from utils.audio import load_voices
 
-
 MODELS_DIR = "/src/.models"
+
+
+import dataclasses
+
+@dataclasses.dataclass
+class Output:
+    output: Path
+    voice: str
 
 
 class Predictor(BasePredictor):
@@ -24,42 +28,21 @@ class Predictor(BasePredictor):
     def setup(self):
         self.tts = TextToSpeech(models_dir=MODELS_DIR)
 
-    def predict(self, text: str = Input(description="Text to prefix with 'hello '")) -> str:
+    def predict(
+        self, 
+        text: str = Input(description="Input text"),
+        voice: str = Input(description="Voice to use", default="random", choices=["random"]),
+        preset: str = Input(description="Preset to use", default="fast", choices=["ultra_fast", "fast", "standard", "high_quality"]),
+        seed: int = Input(description="Seed for deterministic generation", default=None),
+        candidates: int = Input(description="Number of candidates to generate", default=1),
+    ) -> Output:
 
-        import time
+        out_dir = Path(tempfile.mkdtemp())
+        out_path = out_dir / f'{voice}.wav'
 
-        t1 = time.time()
+        voice_samples, conditioning_latents = load_voices([voice])
 
-        text = f"Hello, I am a large language model named {text}"
-        voice = "random"
-        preset = "fast"
-        output_path = "/src/results/"
-        model_dir = "/src/models"
-        candidates = 3
-        seed = None
-        produce_debug_state = True
-        cvvp_amount = 0.0
-        
-        selected_voices = voice.split(',')
-
-        k = 0
-        selected_voice = selected_voices[0]
-        
-        #for k, selected_voice in enumerate(selected_voices):
-        print("VOICE ", k)
-
-        t2 = time.time()
-
-        if '&' in selected_voice:
-            voice_sel = selected_voice.split('&')
-        else:
-            voice_sel = [selected_voice]
-        
-        voice_samples, conditioning_latents = load_voices(voice_sel)
-
-        t3 = time.time()
-
-        gen, dbg_state, _ = self.tts.tts_with_preset(
+        gen = self.tts.tts_with_preset(
             text, 
             k=candidates, 
             voice_samples=voice_samples, 
@@ -67,29 +50,12 @@ class Predictor(BasePredictor):
             preset=preset, 
             use_deterministic_seed=seed, 
             return_deterministic_state=True, 
-            cvvp_amount=cvvp_amount
+            cvvp_amount=0.0
         )
+
+        torchaudio.save(out_path, gen.squeeze(0).cpu(), 24000)
         
-        t4 = time.time()
-
-        if isinstance(gen, list):
-            for j, g in enumerate(gen):
-                torchaudio.save(os.path.join(output_path, f'{selected_voice}_{k}_{j}.wav'), g.squeeze(0).cpu(), 24000)
-        else:
-            torchaudio.save(os.path.join(output_path, f'{selected_voice}_{k}.wav'), gen.squeeze(0).cpu(), 24000)
-        
-        t5 = time.time()
-
-        if produce_debug_state:
-            os.makedirs('debug_states', exist_ok=True)
-            torch.save(dbg_state, f'debug_states/do_tts_debug_{selected_voice}.pth')
-
-        t6 = time.time()
-
-        print(f"t2-t1: {t2-t1}")
-        print(f"t3-t2: {t3-t2}")
-        print(f"t4-t3: {t4-t3}")
-        print(f"t5-t4: {t5-t4}")
-        print(f"t6-t5: {t6-t5}")
-
-        return os.path.join(output_path, f'{selected_voice}_{k}.wav')
+        return Output(
+            output=out_path
+            voice=voice
+        )
